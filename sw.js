a5812a14a7198fe310599ad791729f15dfcd048a
mirhanbeydogan56 <mirhanbeydogan56@gmail.com>
2025-12-13 18:25:36 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,36 @@
-importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging-compat.js');
+const CACHE_NAME = "hogachat-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
 
-firebase.initializeApp({
-  apiKey: "AIzaSyDxr-d1ZKp3wt3i289o6FKmevr3s8Uw3WA",
-  authDomain: "okuler.firebaseapp.com",
-  projectId: "okuler",
-  messagingSenderId: "853942864718",
-  appId: "1:853942864718:web:337671e3596467ae7e6c1f"
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
 
-const messaging = firebase.messaging();
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
 
-messaging.onBackgroundMessage(payload => {
-  self.registration.showNotification(
-    "Neue Nachricht",
-    {
-      body: "Im SchulensteinChat gibt es eine neue Nachricht",
-      icon: "icon-192.png"
-    }
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
